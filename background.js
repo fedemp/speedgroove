a63bd627afc314480a7129d47caefda17ed35962
Federico Panico <bpm230@gmail.com>
2013-05-14 14:49:52 -0300

    Basic logic for listening to events

---
+++ background.js
@@ -0,0 +1,62 @@
+(function(opera, window, document){
+	"use strict";
+
+	/*
+	 * Check what is currently playing in Grooveshark from Opera Speed Dial.
+	 *
+	 * @module SpeedGroove
+	 */
+	var SpeedGroove = {
+
+		/**
+		 * Save a reference to current song.
+		 *
+		 * @property song
+		 */
+
+		song: undefined,
+
+		/**
+		 * Listen to messages from injected script.
+		 *
+		 * @method listen
+		 * @param {Object} message An object that represents the message. It must contain a `topic` and a `body`.
+		 */
+
+		listen: function(message) {
+
+			var topic = message.data.topic;
+			if ( typeof SpeedGroove[topic] == 'function' ) {
+				SpeedGroove[topic](message);
+			}
+		},
+
+		/**
+		 * Handle changes in play status.
+		 *
+		 * @method handlePlayStatus
+		 * @param {Object} message Original message sent by injected script.
+		 */
+
+		handlePlayStatus: function(message) {
+			var currentSong = message.data.body.song;
+			var status = message.data.body.status;
+
+			if (!this.song || currentSong.songID !== this.song.songID) { 
+				this.song = currentSong;		
+			}
+			
+		},
+
+		/**
+		 * Init the app.
+		 *
+		 * @method init
+		 */
+		init: function(){
+			opera.extension.addEventListener('message', this.listen, false);
+		}
+	};
+
+	SpeedGroove.init();
+})(opera, window, window.document);
