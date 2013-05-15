b9bad8fe06752c633c6aa9c09b004b90e209c55c
Federico Panico <bpm230@gmail.com>
2013-05-15 18:03:49 -0300

    Basic logic including listener and updating

--- background.js
+++ background.js
@@ -1,63 +1,77 @@
-(function(opera, window, document){
+(function(opera, window, document, Hogan){
 	"use strict";
+	if (!Hogan) {
+		throw new Error('Hogan is missing');
+	}
 
 	/*
 	 * Check what is currently playing in Grooveshark from Opera Speed Dial.
 	 *
 	 * @module SpeedGroove
 	 */
-	var SpeedGroove = {
+	var SpeedGroove = {};
+	var content = document.getElementById('inner-container');
 
-		bgApp: {
-			/**
-			 * Save a reference to current song.
-			 *
-			 * @property song
-			 */
+	SpeedGroove.bgApp = {
+		/**
+		 * Save a reference to current song.
+		 *
+		 * @property song
+		 */
+		song: undefined,
 
-			song: undefined,
+		/**
+		 * Precompiled template for showing currently playing song.
+		 *
+		 * @property template
+		 */
+		template: (function(){
+			var rawTemplate = '<div><img src="{{artURL}}" alt="{{albumName}}"><br><b class="songName">{{songName}}</b><br><span class="artistName">{{artistName}}</span></div>';
+			return Hogan.compile(rawTemplate);
+		})(),
 
-			/**
-			 * Listen to messages from injected script.
-			 *
-			 * @method listen
-			 * @param {Object} message An object that represents the message. It must contain a `topic` and a `body`.
-			 */
+		/**
+		 * Listen to messages from injected script.
+		 *
+		 * @method listen
+		 * @param {Object} message An object that represents the message. It must contain a `topic` and a `body`.
+		 */
 
-			listen: function(message) {
+		listen: function(message) {
+			var topic = message.data.topic;
+			if ( typeof SpeedGroove.bgApp[topic] == 'function' ) {
+				SpeedGroove.bgApp[topic](message);
+			}
+		},
 
-				var topic = message.data.topic;
-				if ( typeof SpeedGroove.bgApp[topic] == 'function' ) {
-					SpeedGroove.bgApp[topic](message);
-				}
-			},
+		/**
+		 * Handle changes in play status.
+		 *
+		 * @method handlePlayStatus
+		 * @param {Object} message Original message sent by injected script.
+		 */
 
-			/**
-			 * Handle changes in play status.
-			 *
-			 * @method handlePlayStatus
-			 * @param {Object} message Original message sent by injected script.
-			 */
+		handlePlayStatus: function(message) {
+			var currentSong = message.data.body.song;
+			var status = message.data.body.status;
 
-			handlePlayStatus: function(message) {
-				var currentSong = message.data.body.song;
-				var status = message.data.body.status;
+			// Got a new song!
+			if (!this.song || currentSong.songID !== this.song.songID) {
+				this.song = currentSong;
+				content.innerHTML = this.template.render(currentSong);
+			}
 
-				if (!this.song || currentSong.songID !== this.song.songID) {
-					this.song = currentSong;
-				}
+		},
 
-			},
-
-			/**
-			 * Init the app.
-			 *
-			 * @method init
-			 */
-			init: function(){
-				opera.extension.addEventListener('message', this.listen, false);
-			} 
+		/**
+		 * Init the app.
+		 *
+		 * @method init
+		 */
+		init: function(){
+			opera.extension.addEventListener('message', this.listen, false);
 		}
-	}; 
+	};
+
 	SpeedGroove.bgApp.init();
-})(opera, window, window.document);
+})(opera, window, window.document, window.Hogan);
