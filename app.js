cbb185618e302866c08e8c870f85c66dc09bf12d
fedemp <bpm230@gmail.com>
2013-05-24 17:53:49 -0300

    Listen only to last injected script. Add needed returns.

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 (function(opera, window, document, Hogan, Stapes){
-	"use strict";
-	if (!Hogan || !Stapes) {
-		throw new Error('Missing dependencies');
-	}
+  "use strict";
+  if (!Hogan || !Stapes) {
+    throw new Error('Missing dependencies');
+  }
 
-	/**
-	 * Check what is currently playing in Grooveshark from Opera Speed Dial.
-	 * @module SpeedGroove
-	 */
-	var SpeedGroove = {};
+  /**
+   * Check what is currently playing in Grooveshark from Opera Speed Dial.
+   * @module SpeedGroove
+   */
+  var SpeedGroove = {};
 
-	/**
-	 * Hold the background script.
-	 * @namespace SpeedGroove
-	 * @class bgApp
-	 */
-	SpeedGroove.bgApp = {};
+  /**
+   * Hold the background script.
+   * @namespace SpeedGroove
+   * @class bgApp
+   */
+  SpeedGroove.bgApp = {};
 
-	var SpeedGrooveModel = Stapes.subclass();
+  var SpeedGrooveModel = Stapes.subclass();
 
-	var SpeedGrooveView = Stapes.subclass({
-		constructor: function(model){
-			this.model = model;
+  var SpeedGrooveView = Stapes.subclass({
+    constructor: function(model){
+      this.model = model;
 
-			this.el = document.getElementById('container-playing');
+      this.el = document.getElementById('container-playing');
 
-			this.template = Hogan.compile( document.getElementById('template-playing').innerHTML );
-		},
+      this.template = Hogan.compile( document.getElementById('template-playing').innerHTML );
+    },
 
-		render: function() {
+    render: function() {
       document.body.classList.add('playing');
-			var song = this.model.get('currentSong');
-			this.el.innerHTML = this.template.render(song);
+      var song = this.model.get('currentSong');
+      this.el.innerHTML = this.template.render(song);
       return this;
-		},
+    },
 
     init: function(){
       document.body.classList.remove('playing');
@@ -47,17 +47,17 @@
     },
 
     __speedDial: opera.contexts.speeddial,
-	});
+  });
 
-	var SpeedGrooveController = Stapes.subclass({
-		constructor: function(){
-			var self = this;
-			this.model = new SpeedGrooveModel();
-			this.view = new SpeedGrooveView(this.model);
+  var SpeedGrooveController = Stapes.subclass({
+    constructor: function(){
+      var self = this;
+      this.model = new SpeedGrooveModel();
+      this.view = new SpeedGrooveView(this.model);
 
-			this.model.on('change', function(){
-				self.view.render();
-			});
+      this.model.on('change', function(){
+        self.view.render();
+      });
 
       this.on('tabClosed', function(tabEvent){
         if (tabEvent.tab.id === this.__tabID) {
@@ -65,19 +65,23 @@
         }
       });
 
-			this.on('message', function(message){
-				var song, topic = message.data.topic;
-				if (topic === 'NEWSONG') {
-					song = message.data.body.song;
-					this.model.set({
-						currentSong: {
-							songName: song.songName,
-							albumName: song.albumName,
-							artURL: song.artURL,
+      this.on('message', function(message){
+        var song, topic = message.data.topic;
+        if (topic === 'NEWSONG') {
+          // Let's assume that the latest injected script is
+          // the only one which we should listen to.
+          if (!this.__tab.port === message.source) { return; }
+          song = message.data.body.song;
+          this.model.set({
+            currentSong: {
+              songName: song.songName,
+              albumName: song.albumName,
+              artURL: song.artURL,
               artistName: song.artistName
-						}
-					});
-				}
+            }
+          });
+          return;
+        }
 
         if (topic === 'INJECTED') {
           opera.extension.tabs.getAll().forEach(function(tab){
@@ -87,21 +91,22 @@
             }
           },this)
           this.view.init();
+          return;
         }
-			});
+      });
 
-			opera.extension.addEventListener('message', function(message){
-				self.emit('message', message);
-			});
+      opera.extension.addEventListener('message', function(message){
+        self.emit('message', message);
+      });
       opera.extension.tabs.addEventListener('close', function(tabEvent){
         self.emit('tabClosed', tabEvent);
       });
-		}, // end of constructor
+    },
 
     __tabID: undefined,
     __tab: undefined
 
-	});
+  });
 
   SpeedGroove.bgApp.SpeedGrooveController = SpeedGrooveController;
   window.SpeedGroove = SpeedGroove;
