472d2d16b222feb8e4fdb50eff4341b95aee68f1
fedemp <bpm230@gmail.com>
2013-07-08 12:17:49 -0300

    Reorganized for documentation

--- app.js
+++ app.js
@@ -1,165 +1,38 @@
 (function(opera, window, document, Hogan, Stapes){
-	"use strict";
+  "use strict";
+  if (!Hogan || !Stapes) {
+    throw new Error('Missing dependencies');
+  }
 
-    // If this script is loaded in "focus GS" mode, do nothing.
-    if (window.location.search == "?focus=true"){
-      return;
+  /**
+   * @description Check what is currently playing in Grooveshark from Opera Speed Dial.
+   * @module SpeedGroove
+   * @main
+   */
+  var SpeedGroove = {};
+
+  /**
+   * @description Background script container.
+   * @class bgApp
+   * @static
+   */
+  SpeedGroove.bgApp = {
+    /**
+     * @description Init the bgApp. Creates a new controller and binds Opera events to controller events.
+     * @method init
+     * @return void
+     */
+    init: function(){
+      var app = new SpeedGroove.mvc.SpeedGrooveController();
+      opera.extension.addEventListener('message', function(message){
+        app.emit('message', message);
+      });
+      opera.extension.tabs.addEventListener('close', function(tabEvent){
+        app.emit('tabClosed', tabEvent);
+      });
     }
+  };
 
-	if (!Hogan || !Stapes) {
-		throw new Error('Missing dependencies');
-	}
+  window.SpeedGroove = SpeedGroove;
 
-	/**
-	* Check what is currently playing in Grooveshark from Opera Speed Dial.
-	* @module SpeedGroove
-	*/
-	window.SpeedGroove = window.SpeedGroove || {};
-
-	/**
-	* Hold the background script.
-	* @namespace SpeedGroove
-	* @class bgApp
-	*/
-	SpeedGroove.bgApp = undefined;
-
-	var SpeedGrooveModel = Stapes.subclass();
-
-	var SpeedGrooveView = Stapes.subclass({
-		constructor: function(model){
-			var foo;
-			this.model = model;
-			this.el = document.getElementById('container-playing');
-			this.template = Hogan.compile( document.getElementById('template-playing').innerHTML );
-			this.setSpeedDialTitle('Grooveshark');
-		},
-		render: function() {
-			document.body.classList.add('playing');
-			var song = this.model.get('currentSong');
-			this.el.innerHTML = this.template.render(song);
-			this.setFocusMode();
-			this.setSpeedDialTitle('On Grooveshark now...');
-			return this;
-		},
-
-		setFocusMode: function(){
-			this.setSpeedDialUrl('index.html?focus=true');
-        },
-
-		init: function(){
-			document.body.classList.remove('playing');
-			this.setSpeedDialUrl(this.constants.gsURL);
-			this.setSpeedDialTitle('Grooveshark');
-			return this;
-		},
-
-		setSpeedDialUrl: function(url){
-			this.constants.speedDial.url = url;
-			return this;
-		},
-
-		setSpeedDialTitle: function(title){
-			this.constants.speedDial.title = title
-			return this;
-		},
-
-		constants: {
-			gsURL: "http://grooveshark.com",
-			speedDial: opera.contexts.speeddial
-		}
-
-	});
-
-	var SpeedGrooveController = Stapes.subclass({
-		constructor: function(){
-			var self = this;
-
-			this.model = new SpeedGrooveModel();
-			this.view = new SpeedGrooveView(this.model);
-			this.session = {};
-
-			this.model.on('change', function(){
-				self.view.render();
-			});
-
-			this.on('tabClosed', function(e){
-				if (e.evt.tab.id === this.session.tabId) {
-					this.view.init();
-					opera.extension.tabs.removeEventListener('close', e.handler);
-				}
-			});
-
-			this.on('message', function(message){
-				var method = 'handle' + message.data.topic;
-				if (typeof this[method] === "function") {
-					this[method](message);
-					return true;
-				}
-				return false;
-			});
-
-			opera.extension.addEventListener('message', function(message){
-				self.emit('message', message);
-			});
-
-			return this;
-		},
-
-		options: {
-			urlRegex: /^http[s]?:\/\/grooveshark\.com.*/,
-			intervalTime: 5000
-		},
-
-		handleINJECTED: function(message){
-			var self = this;
-			opera.extension.tabs.getAll().forEach(function(tab){
-				if (tab.port === message.source) {
-					this.session.tab = tab;
-					this.session.tabId = tab.id;
-				}
-			},this);
-
-			// Listen to closing tabs only if the injected script
-			// is active.
-			opera.extension.tabs.addEventListener('close', function handleTabClose(tabEvent){
-				self.emit('tabClosed', {evt: tabEvent, handler: handleTabClose});
-			});
-
-			// This is more like a re-init. If we get a new "Injected"
-			// message, then we set the view to its starting point again.
-			this.view.init();
-			this.view.setFocusMode();
-		},
-
-		handleNEWSONG: function(message){
-			// Let's assume that the latest injected script is
-			// the only one which we should listen to.
-			if (!(this.session.tab.port === message.source)) { return; }
-			var song = message.data.body.song;
-			this.model.set({
-				currentSong: {
-					songName: song.songName,
-					albumName: song.albumName,
-					albumID: song.albumID,
-					artistName: song.artistName
-				}
-			});
-			return;
-		},
-
-		pingTabs: function(){
-			var urlRegEx = this.options.urlRegEx;
-			var self = this;
-
-			var intervalID = window.setInterval( function(){
-				if (self.info.tab && !urlRegEx.test(self.info.tab.url)) {
-					self.view.init();
-					window.clearInterval(intervalID);
-				}
-			},this.options.intervalTime);
-		},
-	});
-
-	SpeedGroove.bgApp = new SpeedGrooveController();
-
-})(opera, window, window.document, window.Hogan, window.Stapes);
+})(opera, window, window.document, Hogan, Stapes);
---
+++ mvc.js
@@ -0,0 +1,195 @@
+(function(opera, window, document, Hogan, Stapes, SpeedGroove){
+  "use strict";
+  if (!Hogan || !Stapes) {
+    throw new Error('Missing dependencies');
+  }
+
+  /*
+   * Abstract MVC componentes start here.
+   * ====================================
+   */
+
+  /**
+   * @description Contains the three main constructors of the MVC.
+   * @submodule SpeedGroove.mvc
+   */
+  SpeedGroove.mvc = {};
+
+  /**
+   * @description Model containing currently playing (or last played) song.
+   * @namespace SpeedGroove.mvc
+   * @class SpeedGrooveModel
+   * @constructor
+   */
+  SpeedGroove.mvc.SpeedGrooveModel = Stapes.subclass();
+
+  /**
+   * @description The view of the mvc. This takes care of rendering the Speed Dial tile
+   * @namespace SpeedGroove.mvc
+   * @class SpeedGrooveView
+   * @constructor
+   */
+  SpeedGroove.mvc.SpeedGrooveView = Stapes.subclass({
+
+    /**
+     * @description Constructor for the view. It binds to a model, caches the rendering
+     * context and compiles the template.
+     * @method constructor
+     * @for SpeedGrooveView
+     * @param {SpeedGrooveModel} model Model to which the view will be binded.
+     * @return void
+     */
+    constructor: function(model){
+      this.model = model;
+
+      this.el = document.getElementById('container-playing');
+
+      this.template = Hogan.compile( document.getElementById('template-playing').innerHTML );
+    },
+
+    /**
+     * @description Process the model to render the view.
+     * @method render
+     * @for SpeedGrooveView
+     * @return {SpeedGrooveView} The view object
+     * @chainable
+     */
+     render: function() {
+      document.body.classList.add('playing');
+      var song = this.model.get('currentSong');
+      this.el.innerHTML = this.template.render(song);
+      return this;
+     },
+
+    /**
+     * @description Resets the view. Hide currently playing song, showing placeholder again
+     * and set link of tile to Grooveshark.
+     * @method init
+     * @for SpeedGrooveView
+     * @return {SpeedGrooveView} The view object
+     * @chainable
+     */
+    init: function(){
+      document.body.classList.remove('playing');
+      this.setSpeedDialUrl('http://grooveshark.com');
+      return this;
+    },
+
+    /**
+     * @description Set Speed Dial url.
+     * @method setSpeedDialUrl
+     * @param url {string} New url
+     * @for SpeedGrooveView
+     * @return {SpeedGrooveView} The view object
+     * @chainable
+     */
+    setSpeedDialUrl: function(url){
+      this.__speedDial.url = url;
+      return this;
+    },
+
+    /**
+     * @description Reference to the `speeddial` object.
+     * @property __speedDial
+     * @for SpeedGrooveView
+     * @type Object
+     * @final
+     */
+    __speedDial: opera.contexts.speeddial,
+  });
+
+  /**
+   * @description The controller in the MVC. It instatiates the model and the view and set events to update the model or the view when needed.
+   * @namespace SpeedGroove.mvc
+   * @class SpeedGrooveController
+   * @constructor
+   */
+  SpeedGroove.mvc.SpeedGrooveController = Stapes.subclass({
+    /**
+     * The constructor functions instatiates a new model and view and binds them
+     * to the controller instance. It also set events to update the view on
+     * model change and update the model on messages from injected script and
+     * tab events.
+     * @method constructor
+     * @for SpeedGrooveController
+     * @return {SpeedGrooveController} New instance of the controller
+     * @chainable
+     */
+     constructor: function(){
+      var self = this;
+      this.model = new SpeedGroove.mvc.SpeedGrooveModel();
+      this.view = new SpeedGroove.mvc.SpeedGrooveView(this.model);
+
+      this.model.on('change', function(){
+        self.view.render();
+      });
+
+      this.on('tabClosed', function(tabEvent){
+        if (tabEvent.tab.id === this.__tabID) {
+          this.view.init();
+        }
+      });
+
+      this.on('message', this.handleMessage);
+
+    },
+
+    /**
+     * Read message topic and act accordingly. It may set the model or save a
+     * reference (in a property) to the tab that is running Grooveshark.
+     * @method handleMessage
+     * @for SpeedGrooveController
+     * @param {object} message The original message event.
+     * @return void
+     */
+    handleMessage: function(){
+      var song, topic = message.data.topic;
+      if (topic === 'NEWSONG') {
+        // Let's assume that the latest injected script is
+        // the only one which we should listen to.
+        if (this.__tab.port !== message.source) { return; }
+        song = message.data.body.song;
+        this.model.set({
+          currentSong: {
+            songName: song.songName,
+            albumName: song.albumName,
+            albumID: song.albumID,
+            artistName: song.artistName
+          }
+        });
+        return;
+      }
+
+      if (topic === 'INJECTED') {
+        opera.extension.tabs.getAll().forEach(function(tab){
+          if (tab.port === message.source) {
+            this.__tab = tab;
+            this.__tabID = tab.id;
+          }
+        },this);
+        this.view.init();
+        return;
+      }
+    },
+
+    /**
+     * Caches the ID of the tab where Grooveshark was loaded.
+     * @property __tabID
+     * @for SpeedGrooveController
+     * @type number
+     * @private
+     */
+    __tabID: undefined,
+
+    /**
+     * Caches the `tab` object where Grooveshark was loaded.
+     * @property __tab
+     * @for SpeedGrooveController
+     * @type object
+     * @private
+     */
+    __tab: undefined
+
+  });
+
+})(opera, window, window.document, Hogan, Stapes);
